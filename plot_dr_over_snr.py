30845cddc577483ebb1bdcd67f4dfeac3fa4b9c7
Timothy Gebhard <mail@timothygebhard.de>
2019-06-13 16:30:34 +0200

    Plot results for different values of ∆t; various minor changes
    * Use custom legend
    * Adjust color and linestyle of grid
    * Change aspect ratio from 1:1 to 2:3

--- plot_dr_over_snr.py
+++ plot_dr_over_snr.py
@@ -1,5 +1,5 @@
 """
-Plot the Detection Ratio over the Injection SNR.
+Plot the detection ratio (DR) over the injection SNR.
 """
 
 # -----------------------------------------------------------------------------
@@ -12,6 +12,7 @@ import numpy as np
 import os
 import time
 
+from matplotlib.lines import Line2D
 from pathlib import Path
 
 
@@ -44,37 +45,48 @@ if __name__ == '__main__':
 
     # Fix the data types
     snr_bins = [(float(a), float(b)) for (a, b) in results['snr_bins']]
-    detection_ratios = np.array(results['detection_ratios']).astype(float)
+    slack_widths = [float(_) for _ in results['detection_ratios'].keys()]
 
     # -------------------------------------------------------------------------
     # Actually make the plot: detection ratio over injection SNR
     # -------------------------------------------------------------------------
 
-    print('Creating plot...', end=' ', flush=True)
-
-    # Plot vertical lines to separate SNR bins
-    for x in np.unique(np.array(snr_bins).flatten()):
-        plt.axvline(x=x, color='lightgray', lw=0.5)
-
-    # Plot the PyCBC threshold for comparison
-    plt.axvline(x=np.sqrt(2 * 5.5**2), color='Crimson', lw=0.75)
+    # Define the markers we will be using for the plot (and make it cyclic)
+    def markers(index):
+        markers_ = ['s', 'v', 'p', 'o', 'D', 'x', '>', '<']
+        return markers_[index % len(markers_)]
 
     # Get the bin centers for plotting
     grid = [np.mean(_) for _ in snr_bins]
 
-    # Plot the linear interpolation
-    plt.plot(grid, detection_ratios, color='C0', lw=1)
+    # Plot the results for different slack_width values (i.e., \Delta t)
+    for i, slack_width in enumerate(sorted(slack_widths)):
 
-    # Plot the data points and the error bars
-    plt.errorbar(x=grid, y=detection_ratios,
-                 xerr=len(grid) * [0.25],
-                 ecolor='C0', ls='none', lw=1.5, zorder=99)
+        print(f'Creating plot for slack_width={slack_width:.3f}...',
+              end=' ', flush=True)
 
-    print('Done!', flush=True)
+        # Get the detection ratios for this slack_width value
+        detection_ratios = results['detection_ratios'][str(slack_width)]
+        detection_ratios = np.array(detection_ratios).astype(float)
+
+        # Make plot (plot step-function and actual data points)
+        plt.plot(grid, detection_ratios, marker=markers(i), ms=2, mew=0.5,
+                 color=f'C{i}', linestyle='None', zorder=99)
+        plt.step(grid, detection_ratios, where='mid', lw=1.0, color=f'C{i}',
+                 zorder=99)
+
+        print('Done!', flush=True)
 
     # -------------------------------------------------------------------------
-    # Set up options for the plot
+    # Set up global options for the plot
     # -------------------------------------------------------------------------
+
+    # Plot additional vertical lines to separate SNR bins
+    for x in np.unique(np.array(snr_bins).flatten()[1::2]):
+        plt.axvline(x=x, color='#D9D9D9', lw=0.5, ls='-')
+
+    # Plot the PyCBC threshold for comparison
+    plt.axvline(x=np.sqrt(2 * 5.5**2), color='Crimson', lw=1.0)
 
     # Set up axes ticks and limits
     plt.xticks(np.linspace(5, 20, 16))
@@ -87,7 +99,15 @@ if __name__ == '__main__':
     plt.ylabel('Detection Ratio', fontsize=8)
 
     # Put a grid on the plot
-    plt.grid(which='both', ls='--', alpha=0.35, lw=0.5)
+    plt.grid(which='both', ls='-', color='#D9D9D9', lw=0.5)
+
+    # Add a custom legend
+    legend_elements = [Line2D([], [], ls='-', ms=2, mew=0.5, lw=1.0,
+                              color=f'C{i}', marker=markers(i),
+                              label=r'$\Delta$t = ' + f'{slack_width:.3f}')
+                       for i, slack_width in enumerate(slack_widths)]
+    plt.legend(handles=legend_elements, loc='lower right',
+               fontsize=6, framealpha=1)
 
     # Adjust label sizes
     plt.tick_params(axis='both', which='major', labelsize=6)
@@ -96,7 +116,7 @@ if __name__ == '__main__':
     # Adjust the plot size
     plt.tight_layout()
     width = 8.6 / 2.54 / 1.015
-    plt.gcf().set_size_inches(width, width, forward=True)
+    plt.gcf().set_size_inches(width, 2 / 3 * width, forward=True)
 
     # Construct path to save this plot
     plots_dir = './plots'
